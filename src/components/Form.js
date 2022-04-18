a2ecc9867fb5213571ae73fe1c8dccddd0e601a9
Kyrian <dr.kyrian.onwe@gmail.com>
2022-04-17 21:25:48 -0500

    commit tree dc22832d6b76103aef2fc804fc8b6525f8d19725

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,30 @@
-import React, { useState } from "react";
+/*import React, { useState } from "react";
 
 function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+  function handleFirstNameChange(event){
+    setFirstName(event.target.value);
+    console.log(firstName);
+  }
+
+  function handleLastNameChange(event){
+    setLastName(event.target.value);
+    console.log(lastName);
+  }*/
+  import React from "react";
+
+  function Form({
+    firstName,
+    lastName,
+    handleFirstNameChange,
+    handleLastNameChange,
+  }) {
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
